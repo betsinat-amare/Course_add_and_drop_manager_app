0f78aebb91ad9fed58ec0222f84fe558ba7163b6
Fenet-Ab <abilufenet@gmail.com>
2025-05-09 13:34:47 +0300

    resourse added

---
+++ app/src/main/java/com/example/course_add_and_drop_manager_app/presentation/components/AddAndDropCard.kt
@@ -0,0 +1,4 @@
+package com.example.course_add_and_drop_manager_app.presentation.components
+
+
+
--- app/src/main/java/com/example/course_add_and_drop_manager_app/ui/theme/Color.kt
+++ app/src/main/java/com/example/course_add_and_drop_manager_app/ui/theme/Color.kt
@@ -2,10 +2,24 @@ package com.example.course_add_and_drop_manager_app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val colorPrimary = Color(0xFF6366F1) // Primary color (Purple-ish)
+val colorSecondary = Color(0xFFA5B4FC) // Secondary color (Light Purple)
+val colorText = Color(0xFF4338CA) // Text color (Deep Purple)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val colorHover = Color(0xFF655BEC) // Hover color (Deep Purple)
+val colorAccent = Color(0xFFA5B4FC) // Accent color (Light Purple)
+val colorGray = Color(0xFF7B6F72) // Gray color (Muted Gray)
+val colorHeading = Color(0xFF312E81) // Heading color (Dark Purple)
+val colorGrayBackground = Color(0xFFE0E7FF) // Gray Background color (Light Gray with Blue tint)
+val colorTextField =Color(0xFFEADDDD)
+val white = Color(0xFFFFFFFF)
+
+
+
+
+
+
+
+
+
+
