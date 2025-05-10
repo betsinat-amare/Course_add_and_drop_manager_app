0eb87dbb32443a5ac003d09a65a4184d106cacb8
selam1630 <fikruselam29@gmail.com>
2025-05-10 04:32:22 +0300

    first commit

--- app/src/main/java/com/example/course_add_and_drop_manager_app/presentation/components/AddAndDropCard.kt
+++
@@ -1,4 +0,0 @@
-package com.example.course_add_and_drop_manager_app.presentation.components
-
-
-
---
+++ app/src/main/java/com/example/course_add_and_drop_manager_app/presentation/components/BackButtonHandler.kt
@@ -0,0 +1,36 @@
+
+package com.example.course_add_and_drop_manager_app.presentation.components
+
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
+
+@Composable
+internal fun SystemBackButtonHandler(
+    enabled: Boolean = true,
+    onBackPressed: () -> Unit
+) {
+    val dispatcher = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher
+
+    // Create a callback and remember it so it's not recreated on recomposition
+    val callback = remember {
+        object : OnBackPressedCallback(enabled) {
+            override fun handleOnBackPressed() {
+                onBackPressed()
+            }
+        }
+    }
+
+    // Update enabled status when recomposing
+    DisposableEffect(dispatcher) {
+        callback.isEnabled = enabled
+        dispatcher?.addCallback(callback)
+
+        onDispose {
+            callback.remove()
+        }
+    }
+}
